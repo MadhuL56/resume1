8355ee9849921d65a5b405e7334a936d8fbc89b9
MadhuL56 <155795791+MadhuL56@users.noreply.github.com>
2024-01-06 10:55:01 +0530

    Add files via upload

---
+++ IWD1 sem exam.js
@@ -0,0 +1,20 @@
+
+    
+    
+    
+    
+    <button id="pressMeButton">Press me to check JavaScript</button>
+    
+    
+        document.getElementById('pressMeButton').addEventListener('click', function() {
+            console.log('JavaScript is running');
+        });
+    
+    
+    
+        document.addEventListener('DOMContentLoaded', function() {
+            console.log('Page is loaded');
+        });
+    
+    
+   
